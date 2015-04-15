9a7b898c22178684b8488e61636bab83d16e4076
rockwotj <rockwotj@rose-hulman.edu>
2015-04-14 21:40:24 -0400

    Added HEAD request tests

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ The proposal is to implement [PEP 3333](https://www.python.org/dev/peps/pep-3333
 1. HEAD Requests
 1. Error Codes: 200, 301, 400, 404, 500, & 501
 1. Request Headers: Host, User-Agent, Connection, Accept, Cookie
-1. Response Headers: Date, Server, Content-Length, Connection, Content-Type, Set-Cookie, Redirect
+1. Response Headers: Date, Server, Content-Length, Connection, Content-Type, Set-Cookie, Location
 1. Persistent Connections: Implemented as in the original project
 1. Multiple Connections: Each connection will be handled in a separate thread.
 1. Implement SSL support
@@ -21,10 +21,10 @@ The proposal is to implement [PEP 3333](https://www.python.org/dev/peps/pep-3333
 Example usage would be similar to [Flask](http://flask.pocoo.org/) or [WebApp2](https://webapp-improved.appspot.com/tutorials/quickstart.html).
 Able to define GET or POST Request Handlers for an arbitrary path
 Access Headers, Error Codes and Cookies
-Be able to write to the body output using a stringbuffer. 
+Be able to write to the body output using a stringbuffer.
 
 ## Proof of Concept:
-As proof of concept, we will implement a basic website that uses every aspect of our framework and uses the [Jinja2](http://jinja.pocoo.org/) templating library to output HTML content, and [SQLite](https://docs.python.org/2/library/sqlite3.html#module-sqlite3) for persistent storage. 
+As proof of concept, we will implement a basic website that uses every aspect of our framework and uses the [Jinja2](http://jinja.pocoo.org/) templating library to output HTML content, and [SQLite](https://docs.python.org/2/library/sqlite3.html#module-sqlite3) for persistent storage.
 
 ## Limitations
 Only able to use the following python modules in the Server implementation:
---
+++ test/head_tests.py
@@ -0,0 +1,89 @@
+import re
+import socket
+
+HOST = 'www.example.com'    # The remote host
+PORT = 80              # The same port as used by the server
+
+### TEST HELPER METHODS ###
+
+def ormap(func, seq):
+    result = False
+    booleans = map(func, seq)
+    for b in booleans:
+        result = bool(b) or result
+    return result
+
+def andmap(func, seq):
+    result = True
+    booleans = map(func, seq)
+    for b in booleans:
+        result = bool(b) and result
+    return result
+
+### START TESTS ###
+
+print '===BEGIN HEAD TEST 1 ==='
+print 'Testing HTTP v1.0 HEAD request'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+request = """HEAD /index.html HTTP/1.0
+
+"""
+s.sendall(request)
+data = s.recv(1024)
+response_lines = data.split('\r\n')
+expected = 'HTTP/1.0 200 OK'
+print 'Expecting line 1 of response to be: ', expected
+print 'Got: ', response_lines[0]
+
+if expected == response_lines[0]:
+    print 'PASSED TEST 1'
+else:
+    print 'FAILED TEST 1'
+s.close()
+
+print '===BEGIN HEAD TEST 2 ==='
+print 'Testing HTTP v1.1 HEAD request'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+request = """HEAD / HTTP/1.1
+Host: example.com
+
+"""
+s.sendall(request)
+data = s.recv(1024)
+response_lines = data.split('\r\n')
+expected = 'HTTP/1.1 200 OK'
+print 'Expecting line 1 of response to be: ', expected
+print 'Got: ', response_lines[0]
+if expected == response_lines[0]:
+    print 'PASSED TEST 2'
+else:
+    print 'FAILED TEST 2'
+
+print '===BEGIN HEAD TEST 3 ==='
+print 'Testing HTTP v1.1 HEAD request with headers'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+request = """HEAD / HTTP/1.1
+Host: example.com
+Accept: text/plain
+Accept-Language: en-us
+
+"""
+s.sendall(request)
+data = s.recv(1024)
+response_lines = data.split('\r\n')
+expected_lines = [  'HTTP/1.1 200 OK',
+                    'Date: (\w\w\w), (\d\d) (\w\w\w) (\d\d\d\d) (\d\d):(\d\d):(\d\d) ([A-Z]+)' ,
+                    'Server: (\w+)',
+                    'Content-Length: (\d+)']
+print 'Expecting lines of response to be: ', expected_lines
+print 'Got: ', response_lines
+test_passed = True
+line_in_resp = lambda expected_line: ormap(lambda line: re.match(expected_line, line), response_lines)
+test_passed = andmap(line_in_resp, expected_lines)
+if test_passed:
+    print 'PASSED TEST 3'
+else:
+    print 'FAILED TEST 3'
