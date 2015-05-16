4de58dbb84e69767ad720020ffaa906a99d1c276
Greg Callegari <calleggr@rose-hulman.edu>
2015-05-16 09:54:39 -0400

    created database file, some work started

---
+++ src/test_app/framework/server/database.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+def connect(name):
+    conn = sqlite3.connect(name) #name of database
+    c = conn.cursor()
+
+def close(c):
+    c.commit()
+    c.close()
+
+
+def createTable(table_name, column_names, c):
+    """create a table in the database
+       takes 3 params, a table_name, list of column_names,
+       and a cursor object"""
+    sql_statement = 'CREATE TABLE ' + table_name + '(INT ID PRIMARY KEY NOT NULL, '
+    for col in column_names:
+        sql_statement += col + ' TEXT NOT NULL, '
+    sql_statement = sql_statement[:-2]
+    sql_statement += ')'
+    c.execute(sql_statement)
+
+def addEntry():
+    pass
+
+def deleteEntry():
+    pass
+
+def updateEntry():
+    pass
