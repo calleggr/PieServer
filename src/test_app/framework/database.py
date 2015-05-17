d1d90604eec219a37dafd56dd18d026025f97657
calleggr <calleggr@rose-hulman.edu>
2015-05-17 14:00:46 -0400

    fixed some errors in read functions

--- src/test_app/framework/database.py
+++ src/test_app/framework/database.py
@@ -45,14 +45,34 @@ def readEntry(table_name, column_name, search, c):
     """SELECT
         selects row from table_name where column_name = search
         c = db cursor"""
-    return c.execute("SELECT * FROM %s where %s=%s;", (table_name, column_name, search))
+    sql_statement = "SELECT * FROM " + table_name + " WHERE " + column_name + "=" + "'" + search + "'" + ";"
+    print sql_statement
+    c.execute(sql_statement)
+    return_str = []
+    i = 0;
+    for row in c:
+        return_str.append([])
+        for val in row:
+            return_str[i].append(str(val))
+        i+=1
+    return return_str
+
 
 def readAll(table_name, c):
     """SELECT
         selects all rows from table_name
         c = db cursor"""
-    table = (table_name,)
-    return c.execute("SELECT * FROM ?", table)
+    sql_statement = "SELECT * FROM " + table_name + ";"
+    c.execute(sql_statement)
+    return_str = []
+    i = 0;
+    for row in c:
+        return_str.append([])
+        for val in row:
+            return_str[i].append(str(val))
+        i+=1
+    return return_str
+
 
 
 def updateEntry(table_name, column_name, update_value, look_up_col, look_up_val, c):
