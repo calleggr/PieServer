5380096d303b840aaed3ad93e7854a9e8df85f77
calleggr <calleggr@rose-hulman.edu>
2015-05-17 23:11:14 -0400

    BASH

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,19 @@
-python src/start_test_server.py &
-python src/start_ssl_server.py
-wait
+#!/bin/bash
+
+
+declare -a arr=("python src/start_test_server.py" "python src/start_ssl_server.py")
+
+for cmd in "${arr[@]}"; do {
+  echo "Process \"$cmd\" started";
+  $cmd & pid=$!
+  PID_LIST+=" $pid";
+} done
+
+trap "kill $PID_LIST" SIGINT
+
+echo "Parallel processes have started";
+
+wait $PID_LIST
+
+echo
+echo "All processes have completed";
